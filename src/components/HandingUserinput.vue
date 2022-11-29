ff44c415227a07d9d3212a099d83c7628e5618cc
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-11-29 14:46:31 +0700

    Reverse Maessage

--- src/components/HandingUserinput.vue
+++ src/components/HandingUserinput.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const message = ref("Hello World");
+function reverseMessage() {
+  console.log(message.value.split("").reverse());
+}
 </script>
 
 <template>
   <h1>{{ message }}</h1>
-  <button>Revere Message</button>
-  <button v-on:click="message += '!'">Append "!"</button>
+  <button @click="reverseMessage">Reverse Message</button>
+  <button @click="message += '!'">Append "!"</button>
   <a href="">A link with e.preventDefault</a>
 </template>
 
