bf268d8549c3c119825ed2c8888ca029f0788536
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-11-29 15:31:34 +0700

    Bigin AttributeBindings

---
+++ src/components/AttributeBinding.vue
@@ -0,0 +1,20 @@
+<script setup lang="vs">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <p>
+    <span title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </p>
+  <p class="red">
+    <span>This should be red... but click me to toggle it.</span>
+  </p>
+  <p style="color: green">
+    <span>This should be red... but click me to toggle it.</span>
+  </p>
+</template>
+
+<style scoped></style>
