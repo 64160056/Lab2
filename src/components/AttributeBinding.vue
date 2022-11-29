c56975d04211fab7b1217b7840c64124c5d0dabe
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2022-11-29 15:52:27 +0700

    Creaet class isRed

--- src/components/AttributeBinding.vue
+++ src/components/AttributeBinding.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const message = ref("Helllo World!");
+const isRed = ref(true);
+function toggleRed() {
+  isRed.value = !isRed.value;
+}
 </script>
 
 <template>
   <p>
-    <span v-bind:title="message">
+    <span :title="message">
       Hover your mouse over me for a few seconds to see my dynamically bound
       title!
     </span>
   </p>
-  <p class="red">
+  <p :class="{ red: isRed }" @click="toggleRed">
     <span>This should be red... but click me to toggle it.</span>
   </p>
   <p style="color: green">
